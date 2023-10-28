d3ad4e17982ae5bada615e63811c8818e91b05d4
Sauito <aikelihtenstein@gmail.com>
2023-10-28 20:24:02 +0300

    add transition

--- app/src/main/java/com/example/snotra/ActivityMain.java
+++ app/src/main/java/com/example/snotra/ActivityMain.java
@@ -3,15 +3,18 @@ package com.example.snotra;
 import android.os.Bundle;
 
 import com.example.snotra.ui.first_aid.first_aid;
+import com.example.snotra.ui.settings.settings;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.app.Activity;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 
@@ -47,6 +50,30 @@ public class ActivityMain extends AppCompatActivity {
         public boolean onCreateOptionsMenu (Menu menu){
             getMenuInflater().inflate(R.menu.top_menu, menu);
             return true;
+        }
+
+        @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+        {
+            int itemId = item.getItemId();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            if (itemId == R.id.item_settings) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment_activity_main, settings.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+                return true;
+            }
+            else if (itemId == R.id.item_first_aid) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment_activity_main, first_aid.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
         }
 
 }
